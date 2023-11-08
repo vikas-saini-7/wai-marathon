b77c35b141e4908d95aa6fd0948c9f349f5e579f
vikas-saini-7 <vikas289888@gmail.com>
2023-11-08 18:26:24 +0530

    changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center py-8 uppercase opacity-100 text-xs pt-16'><span className='opacity-50'>Built & developed by</span> <a className='opacity-100' href="">Vikas Saini</a></div>
+    <div className='text-center py-8 uppercase opacity-100 text-xs pt-16'><span className='opacity-50'>Built & developed by</span> <a target='_blank' className='opacity-100' href="https://www.linkedin.com/in/vikas-saini-602b96227/">Vikas Saini</a></div>
   )
 }
 
